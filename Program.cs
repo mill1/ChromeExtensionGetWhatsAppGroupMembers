d6f9ab4352c91c98ea010d0b2e7b712c164954c6
Emiel Nijhuis <emiel.nijhuis@speerit.nl>
2025-08-31 21:22:36 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Blazor.BrowserExtension;
+using ChromeExtensionGetWhatsAppGroupMembers;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.UseBrowserExtension(browserExtension =>
+{
+    if (browserExtension.Mode == BrowserExtensionMode.Background)
+    {
+        builder.RootComponents.AddBackgroundWorker<BackgroundWorker>();
+    }
+    else
+    {
+        builder.RootComponents.Add<App>("#app");
+        builder.RootComponents.Add<HeadOutlet>("head::after");
+    }
+});
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+await builder.Build().RunAsync();
