1574f1b2a23fbaac6554dd4d245294a8e48a0136
mill1 <enijhuis@outlook.com>
2025-09-06 22:06:42 +0200

    browserextension-v1.0.1.zip: debug -> release

--- wwwroot/memberWindow.js
+++ wwwroot/memberWindow.js
@@ -1,8 +1,8 @@
 ﻿window.openMemberWindow = function (member) {
-    const win = window.open("", "_blank", "width=300,height=150");
+    const win = window.open("", "_blank", "width=300,height=165");
     if (win) {
         win.document.write("<!DOCTYPE html><html><head><title>Member</title></head><body>");
-        win.document.write("<h2 style='font-family: Arial; text-align: center; margin-top: 50px;'>Member: " + member + "</h2>");
+        win.document.write("<h2 style='font-family: Helvetica, Arial; color: #20b958; text-align: center; margin-top: 50px;'>Member: " + member + "</h2>");
         win.document.write("</body></html>");
         win.document.close();
         win.focus();
