192c5ebdfb45becf34d501058075d7c731431fa9
Emiel Nijhuis <emiel.nijhuis@speerit.nl>
2025-09-06 21:32:22 +0200

    Add member popup window

---
+++ wwwroot/memberWindow.js
@@ -0,0 +1,12 @@
+﻿window.openMemberWindow = function (member) {
+    const win = window.open("", "_blank", "width=300,height=150");
+    if (win) {
+        win.document.write("<!DOCTYPE html><html><head><title>Member</title></head><body>");
+        win.document.write("<h2 style='font-family: Arial; text-align: center; margin-top: 50px;'>Member: " + member + "</h2>");
+        win.document.write("</body></html>");
+        win.document.close();
+        win.focus();
+    } else {
+        alert("Popup blocked! Please allow popups for this extension.");
+    }
+};
